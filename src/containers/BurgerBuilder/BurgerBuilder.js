1562b77d4b5e79139968b4c3fca4bd2efe8a145b
akshatcr7 <imakshat147@gmail.com>
2020-04-20 22:40:15 +0530

    Fixing a bug

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,7 +35,7 @@ class BurgerBuilder extends Component {
         .reduce((sum,el) => {
             return sum + el;
         },0);
-        this.setState({purchasable: sum > 0});
+        return sum > 0;
     }
 
     // addIngredientHandler = (type) => {
@@ -119,7 +119,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ing)}
                         ordered={this.purchaseHandler}/>
                 </Aux>
             );
